cd377f6d8e3812bbbec88cf089e2dba29d7b2649
Kübra Bol <kubra1272@icloud.com>
2023-09-07 02:08:03 +0300

    update

--- DataAccess/Concrete/EntityFremework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFremework/EfCarDal.cs
@@ -23,13 +23,13 @@ namespace DataAccess.Concrete.EntityFremework
                 var result = from c in context.Cars
                              join b in context.Brands
                              on c.BrandId equals b.BrandId
-                             //join co in context.Colors
-                             //on c.ColorId equals co.ColorId
+                             join co in context.Colors
+                             on c.ColorId equals co.ColorId
                              select new CarDetailDto()
                              {
                                  CarName = c.CarName,
                                  BrandName = b.BrandName,
-                                 //ColorName = co.ColorName,
+                                 ColorName = co.ColorName,
                                  DailyPrice = c.DailyPrice
                              };
                 return result.ToList();
